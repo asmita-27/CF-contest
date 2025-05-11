34dc7babc313743a098fc0423caf41bce20bacd1
ASMITA <asmitapawar3113@gmail.com>
2025-05-11 20:31:08 +0530

    Codeforces Round 1024 (Div. 2)

---
+++ dinnerTIme.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n, m, p, q = map(int, input().split())
+    if n % p != 0:
+        print("YES")
+    else: 
+        if m == (n // p) * q:
+            print("YES")
+        else:
+            print("NO")

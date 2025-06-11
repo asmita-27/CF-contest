b255299e8b8bebf800b3824f1a5df79456eaee0d
ASMITA <asmitapawar3113@gmail.com>
2025-06-11 18:57:49 +0530

    daily

--- D_Coprime.py
+++ D_Coprime.py
@@ -3,19 +3,19 @@ import os
 import math
 import sys
 
-
+  
 for _ in range(int(input())):
-    pass
     n = int(input())
     a = list(map(int, input().split()))
-    res = 0
-    i,j = n-1, n-1
-    while i >  0 and j >  0:
-        if math.gcd(a[i], a[j]) == 1:
-            res = max(res, i+j+2)
-            j -= 1
-            i -= 1
-         
- 
-    print(res if res > 0 else -1)
-            
+
+    pos = {} 
+    for id, val in enumerate(a):
+        pos[val] = id
+    res = -1
+    nums = list(pos.keys())
+
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if math.gcd(nums[i], nums[j]) == 1:
+                res = max(res, pos[nums[i]] + pos[nums[j]] + 2)   
+    print(res)

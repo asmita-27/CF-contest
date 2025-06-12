391bfb43ab29042abaa63b35880e8774e882c8d8
ASMITA <asmitapawar3113@gmail.com>
2025-06-12 19:35:28 +0530

    12/06

--- B_Taxi.py
+++ B_Taxi.py
@@ -5,17 +5,20 @@ import sys
 
 n = int(input())
 s =  list(map(int, input().split()))
-cnt =  0
-stk = []
-s.sort(reverse=True)
-for i in s :
-    if i== 4:
-        cnt+=1 
-    else:
-        while stk and i + stk[-1] <= 4:
-            cnt+=1
-            stk.pop()
-        if i != 4:
-            stk.append(i)
-print(cnt)
-    
+
+freq = Counter(s)
+ans = 0
+
+ans += freq[4]
+
+ans += freq[3]
+freq[1] = max(0, freq[1] - freq[3])
+
+ans += (freq[2] + 1) // 2
+if freq[2] % 2 == 1:
+    freq[1] = max(0, freq[1] - 2)
+
+ans += (freq[1] + 3) // 4
+
+print(ans)
+

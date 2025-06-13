0dfecebe18aba19b6eab3e6285b0c4c356d75f3a
ASMITA <asmitapawar3113@gmail.com>
2025-06-13 18:32:15 +0530

    13/06

---
+++ C_Make_It_Beautiful.py
@@ -0,0 +1,19 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    pass
+    n , k = map(int, input().split())
+    a = list(map(int, input().split()))
+    ans = 0
+    a.sort()
+    while k!=0:
+        for i in a :
+            if i%2==0:
+                i+=1
+                k-=1
+    ans = sum(i.bit_count() for i in a)
+    print(ans)

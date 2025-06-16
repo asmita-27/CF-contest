1dda9cc46fbfbc7685fb00380eaeb965aa356996
ASMITA <asmitapawar3113@gmail.com>
2025-06-16 19:22:06 +0530

    16/06

--- B_Maximum_Sum.py
+++ B_Maximum_Sum.py
@@ -11,16 +11,15 @@ for _ in range(int(input())):
     ans = sum(a)
     while k > 0:
         a.sort()
-        if a[0]+a[1]<= a[-1] :
+        if a[0]+a[1]<= a[-1] or a[-1] > ans:
             ans -= (a[0] + a[1])
             a.pop(0)
             a.pop(0)
         elif a[0]+a[1]>a[-1] or a[-1]<=ans:
             ans -= a[-1]
             a.pop()
-        
+            
         k -= 1
-        
 
     print(ans)
 

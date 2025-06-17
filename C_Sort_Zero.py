a3f27a38a256f0a28fd3c6e2ee412af9c6261593
ASMITA <asmitapawar3113@gmail.com>
2025-06-17 19:07:36 +0530

    17/06

--- C_Sort_Zero.py
+++ C_Sort_Zero.py
@@ -8,13 +8,12 @@ for _ in range(int(input())):
     pass
     n = int(input())
     a = list(map(int,input().split()))
-    cnt = Counter(a)
-    ans = 0
-    for i in range(1,n):
-        if a[i] in cnt and cnt[a[i]]>=1:
-            ans += 1
-            cnt[a[i]] = 0
-        else:
-            
     
+    ans = 0
+    for i in a:
+        cnt = Counter(a)
+        if i in cnt and cnt[i]>=1:
+            ans += 1
+            a.remove(i)
+
     print(ans)

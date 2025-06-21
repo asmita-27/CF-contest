fc8c98d5940c2709facbb5198d642fc9db9d0df4
ASMITA <asmitapawar3113@gmail.com>
2025-06-21 18:37:47 +0530

    21/06

--- C_Save_the_Magazines.py
+++ C_Save_the_Magazines.py
@@ -5,5 +5,20 @@ import sys
 
 
 for _ in range(int(input())):
-    pass
-
+	n = int(input())
+	s = '0' + input()
+	a = [0] + list(map(int, input().split()))
+	ans = 0
+	i = 0
+	while i <= n:
+		mn = a[i]
+		sm = a[i]
+		j = i + 1
+		while j <= n and s[j] == '1':
+			mn = min(mn, a[j])
+			sm += a[j]
+			j += 1
+		ans += sm - mn
+		i = j
+	print(ans)
+	

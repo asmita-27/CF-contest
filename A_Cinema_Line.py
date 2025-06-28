4c88455cf04df5934a80c38cb5d15360a00d4bd1
ASMITA <asmitapawar3113@gmail.com>
2025-06-28 18:54:19 +0530

    28/06

--- A_Cinema_Line.py
+++ A_Cinema_Line.py
@@ -12,6 +12,23 @@ for i in range(1, n):
     if a[0]!=25:
         flg = 1
         continue
+    if a[i] == 25:
+        cnt += 25
+    elif a[i] == 50  :
+        if cnt < 25:
+            flg = 1
+            continue
+        cnt -= 25
+        cnt += 50
+    elif a[i] == 100:
+        if cnt < 75:
+            flg = 1
+            continue
+        if cnt >= 75:
+            cnt -= 75
+            cnt += 100
+        else:
+            cnt -= 25
 
 if flg:
     print("NO")

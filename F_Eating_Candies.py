460fd8ced4b87a9e1bd5f2a17d92306d20cf9ba5
ASMITA <asmitapawar3113@gmail.com>
2025-07-03 18:14:57 +0530

    3/07

--- F_Eating_Candies.py
+++ F_Eating_Candies.py
@@ -7,4 +7,20 @@ import sys
 for _ in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
-    
+    l,r  = 0, n - 1
+    sumL = a[0]
+    sumR = a[n-1]
+    ans = 0
+    while l < r:
+        if sumL == sumR:
+            ans = max(ans, l + 1 + n - r)
+
+        if sumL <= sumR:
+            l+=1
+            sumL+=a[l]
+
+        elif sumR < sumL:
+            r-=1
+            sumR+=a[r]
+
+    print(ans)

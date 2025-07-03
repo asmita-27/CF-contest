2de4bdc47a6430656c63b8b36669c1ba1010070f
ASMITA <asmitapawar3113@gmail.com>
2025-07-03 18:07:28 +0530

    3/07

---
+++ F_Eating_Candies.py
@@ -0,0 +1,10 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    

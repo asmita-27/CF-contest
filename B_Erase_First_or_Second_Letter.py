d2154115583e76031c1b5e3156da20de64e05ae3
ASMITA <asmitapawar3113@gmail.com>
2025-07-04 20:17:43 +0530

    4/07

--- B_Erase_First_or_Second_Letter.py
+++ B_Erase_First_or_Second_Letter.py
@@ -7,5 +7,15 @@ import sys
 for _ in range(int(input())):
     n = int(input())
     s = input().strip()
-    
+    ans = [0] * n
+    nxt = [n] * 26  
+    ans[n - 1] = 1
+    nxt[ord(s[n - 1]) - ord('a')] = n - 1
 
+    for i in range(n - 2, -1, -1):
+        ans[i] = ans[i + 1] + (nxt[ord(s[i]) - ord('a')] - i)
+        nxt[ord(s[i]) - ord('a')] = i
+    
+    print(ans[0])
+
+    

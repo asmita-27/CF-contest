e5a1d23498948f5a93e885eb110a0c5bed0f7bc8
ASMITA <asmitapawar3113@gmail.com>
2025-07-07 20:59:37 +0530

    07/07

--- C_A_Good_Problem.py
+++ C_A_Good_Problem.py
@@ -7,5 +7,25 @@ import sys
 for _ in range(int(input())):
     n,l,r,k = map(int, input().split())
     
+    if n==1:
+        print(l)
+        continue
+    if n%2==1:
+        print(l)
+    
+    else:
+        if n==2:
+            print(-1)
+
+        else:
+            v = l.bit_length()
+            x = 2**v
+            if x not in range(l, r+1):
+                print(-1)
+            else:
+                if k<=n-2:
+                    print(l)
+                else:
+                    print(x)
     
  

bc02147d0246a03213c9375b54ce6196f2a319fb
ASMITA <asmitapawar3113@gmail.com>
2025-07-08 22:09:33 +0530

    8/07

---
+++ B_Minimise_Sum.py
@@ -0,0 +1,12 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    print(min(a[0], a[1] ) + a[0])
+    

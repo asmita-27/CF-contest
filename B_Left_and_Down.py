1441f26c33079af36cf36817934593d7415becb2
ASMITA <asmitapawar3113@gmail.com>
2025-07-22 23:02:59 +0530

    22/07

--- B_Left_and_Down.py
+++ B_Left_and_Down.py
@@ -1,14 +1,13 @@
 from collections import defaultdict, Counter, deque
 import os
-import math
 import sys
-
+import math
 
 for _ in range(int(input())):
     a, b, k = map(int, input().split())
     g = math.gcd(a, b)
-    mn = max(a// k, b // k)
-    if g> mn:
+    mn = max((a+k-1)// k, (b+k-1) // k)
+    if g>= mn:
         print(1)
     else:
         print(2)

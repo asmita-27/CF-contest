7b690f5dcb33e7dce5cbdb5312c9a920b44bbca5
ASMITA <asmitapawar3113@gmail.com>
2025-09-14 20:54:56 +0530

    14/09

--- A_Shift_Sort.py
+++ A_Shift_Sort.py
@@ -6,4 +6,8 @@ import sys
 
 for _ in range(int(input())):
     pass
-
+    n = int(input())
+    s = input()
+    sSrt = ''.join(sorted(s))
+    diff = sum(1 for i in range(len(s)) if s[i] != sSrt[i])
+    print(diff//2)
---
+++ B_Another_Divisibility_Problem.py
@@ -0,0 +1,9 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    x = int(input())
+    print(2 * x)

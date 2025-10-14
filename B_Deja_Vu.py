22204d4975b563f92b7b6e2df1499b7772d8f9e9
ASMITA <asmitapawar3113@gmail.com>
2025-10-14 12:43:26 +0530

    14/10

--- B_Deja_Vu.py
+++ B_Deja_Vu.py
@@ -7,12 +7,17 @@ import sys
 for _ in range(int(input())):
     n,q = map(int, input().split())
     a = list(map(int, input().split()))
-    aQ = list(map(int, input().split()))
-    minX = 31
-    for x in aQ:
-        add = 1 << (x - 1)   
-        div = 1 << x      
-        for i in range(n):
-            if a[i] % div == 0:
-                a[i] += add
+    queries = list(map(int, input().split()))
+
+    used = [False] * 31   
+    for x in queries:
+        if not used[x]:
+            add_val = 1 << (x - 1)
+            div_val = 1 << x
+            for i in range(n):
+                if a[i] % div_val == 0:
+                    a[i] += add_val
+            used[x] = True 
+            for j in range(x + 1, 31):
+                used[j] = True  
     print(*a)

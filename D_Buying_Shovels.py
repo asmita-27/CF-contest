52717e0b936b8ab7fe1a481e0454b4ff84ad4b4a
ASMITA <asmitapawar3113@gmail.com>
2025-11-23 11:29:21 +0530

    23/11

---
+++ D_Buying_Shovels.py
@@ -0,0 +1,18 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    ans = n
+    j =1 
+    while j*j <= n:
+        if n % j == 0:
+            if j <= k:
+                ans = min(ans, n // j)
+            if n // j <= k:
+                ans = min(ans, j)
+        j += 1
+    print(ans)

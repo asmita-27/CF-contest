1f27d0ce34a0edf6bc262335c494d2ff0a11a9c6
ASMITA <asmitapawar3113@gmail.com>
2025-11-27 19:56:49 +0530

    27/11

---
+++ A_Strange_Birthday_Party.py
@@ -0,0 +1,20 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    k = list(map(int, input().split()))
+    c = list(map(int, input().split()))
+    k.sort(reverse=True)
+    ptr = 0
+    tot =0
+    for i in k:
+        if ptr < m and c[ptr] <c[i-1]:
+            tot += c[ptr]
+            ptr += 1
+        else:  
+            tot += c[i-1]
+    print(tot)

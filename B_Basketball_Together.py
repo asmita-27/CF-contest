9815142d4bcc945f8a2a8b92ff0c4e54448e175f
ASMITA <asmitapawar3113@gmail.com>
2025-12-06 22:01:05 +0530

    06/12

--- B_Basketball_Together.py
+++ B_Basketball_Together.py
@@ -3,13 +3,19 @@ import os
 import math
 import sys
 
-n,d = map(int, input().split())
-a = list(map(int, input().split()))
-a.sort()
-ans = wn= 0
-for i in reversed(a):
-    ans+=1
-    if ans*i >= d:
-        wn+=1
-        ans =0
-print(wn)
+
+
+
+n, D = map(int, input().split())
+P = sorted(map(int, input().split()))
+
+wins = 0
+team = []
+
+for p in P:  
+    team.append(p)
+    if team[-1] * len(team) > D:
+        wins += 1
+        team = []
+
+print(wins)

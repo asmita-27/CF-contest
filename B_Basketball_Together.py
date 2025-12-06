ae4c90ce97c4cf9b517b4dd2287df0eeb9b27a12
ASMITA <asmitapawar3113@gmail.com>
2025-12-06 22:06:33 +0530

    06/12

--- B_Basketball_Together.py
+++ B_Basketball_Together.py
@@ -3,21 +3,19 @@ import os
 import math
 import sys
 
-
 n, D = map(int, input().split())
 P = sorted(map(int, input().split()))
 
 wins = 0
-used = 0
-i = n - 1
-
-while i >= 0:
-    x = P[i]
+l = 0
+r = n - 1
+while l <= r:
+    x = P[r]
     need = D // x + 1
-    if i + 1 - used < need:
+    if l + (need - 1) > r:
         break
-    used += need
     wins += 1
-    i -= 1
+    l += need - 1
+    r -= 1
 
 print(wins)

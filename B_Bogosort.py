7d9b896fa51c6ebfe3d8332759687ad6ba53b0e0
ASMITA <asmitapawar3113@gmail.com>
2025-12-09 11:13:59 +0530

    09/12

---
+++ B_Bogosort.py
@@ -0,0 +1,10 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n = int(input())
+    print(*sorted(map(int, input().split()))[::-1])
+

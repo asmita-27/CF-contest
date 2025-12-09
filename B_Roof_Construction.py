6517fd6eddd22613cb5cf2b9510ee04fd5c75b05
ASMITA <asmitapawar3113@gmail.com>
2025-12-09 11:08:05 +0530

    09/12

---
+++ B_Roof_Construction.py
@@ -0,0 +1,15 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+for _ in range(int(input())):
+	n = int(input())
+	k = 1
+	while 2 * k <= n - 1:
+		k *= 2
+	for i in range(k - 1, -1, -1):
+		print(i, end=' ')
+	for i in range(k, n):
+		print(i, end=' ')
+	print()

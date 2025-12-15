dd7f2e330f370fe68c2175a6408c429383ded3a4
ASMITA <asmitapawar3113@gmail.com>
2025-12-15 18:00:49 +0530

    15/12

--- B_Beautiful_String.py
+++ B_Beautiful_String.py
@@ -3,27 +3,14 @@
 for _ in range(int(input())):
     n = int(input())
     s = input().strip()
-    flg = False
-    for mask in range(1 << n):
-        p = []
-        idx = []
-        x = []
-        for i in range(n):
-            if (mask >> i) & 1:
-                p.append(s[i])
-                idx.append(i + 1)
-            else:
-                x.append(s[i])
-        if p != sorted(p):           
-            continue
-        x = ''.join(x)
-        if x == x[::-1]:              
-            print(len(idx))
-            if idx:
-                print(*idx)
-            else:
-                print()              
-            flg = True
-            break
-    if not flg:
-        print(-1)
+
+    pos = []
+    for i in range(n):
+        if s[i] == '0':
+            pos.append(i + 1)
+
+    print(len(pos))
+    if pos:
+        print(*pos)
+    else:
+        print()

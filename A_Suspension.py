71ae508b820397b5af2de5b83856de0b9e9d41de
ASMITA <asmitapawar3113@gmail.com>
2026-01-01 17:26:05 +0530

    31/12

---
+++ A_Suspension.py
@@ -0,0 +1,30 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n = int(input())
+    y,r = map(int, input().split())
+    ans = 0
+    if y == 0 and r == 0:
+        print(0)
+        continue
+ 
+    if y // 2 >= r: 
+        ans += y // 2 
+        n -= y // 2 
+ 
+        if n > 0: 
+            ans += min(n, r) 
+            n -= min(n, r)
+    else:
+        ans += r 
+        n -= r
+        if n > 0: 
+            ans += min(n, y // 2) 
+            n -= min(n, y // 2)
+    
+    print(ans)
+ 

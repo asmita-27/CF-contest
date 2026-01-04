167d5bfae6afcdb161d87d813b4153ccf8c85f87
ASMITA <asmitapawar3113@gmail.com>
2026-01-04 17:49:44 +0530

    04/01

--- B_Siga_ta_Kymata.py
+++ B_Siga_ta_Kymata.py
@@ -3,6 +3,35 @@ import os
 import math
 import sys
 
+ 
+def solve():
+    n = int(input())
+    p = list(map(int, input().split()))
+    s = input()
+ 
+    mn = 0
+    mx = 0
+    for i in range(n):
+        if p[i] < p[mn]:
+            mn = i
+        if p[i] > p[mx]:
+            mx = i
 
+    if s[0] == "1" or s[-1] == "1":
+        print(-1)
+        return
+
+    if s[mn] == "1" or s[mx] == "1":
+        print(-1)
+        return
+ 
+    print(5)
+    print(1, mn + 1)
+    print(1, mx + 1)
+    print(mn + 1, n)
+    print(mx + 1, n)
+    print(min(mn + 1, mx + 1), max(mn + 1, mx + 1))
+ 
+ 
 for _ in range(int(input())):
-    
+    solve()

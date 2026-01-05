84db9a8d86e087b4e8276c1eaa8d0d7548700e7e
ASMITA <asmitapawar3113@gmail.com>
2026-01-05 18:35:02 +0530

    05/01

---
+++ A_Sequence_Game.py
@@ -0,0 +1,15 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n =  int(input())
+    a = list(map(int, input().split()))
+    x = int(input())
+    
+    if max(a)<x:
+        print("NO")
+    else:
+        print("YES")

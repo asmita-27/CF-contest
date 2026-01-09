a26437b21effb745a7805a6890d83863f1bcfd48
ASMITA <asmitapawar3113@gmail.com>
2026-01-09 21:05:15 +0530

    09/01

--- B_Make_it_Zigzag.py
+++ B_Make_it_Zigzag.py
@@ -5,5 +5,18 @@ import sys
 
 
 for _ in range(int(input())):
-    pass
-
+    n = int(input())
+    a = list(map(int,input().split()))
+    pre = [0]*(n)
+    res =0
+    pre[0] = a[0]
+    for i in range(n):
+        pre[i] = max(pre[i-1],a[i])
+    for i in range(0,n,2):
+        dif = -1
+        if i>0:
+            dif = max(dif, a[i]-pre[i-1])
+        if i<n-1:
+            dif = max(dif, a[i]-pre[i+1])
+        res += dif+1
+    print(res)

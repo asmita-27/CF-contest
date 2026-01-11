1ee80cccb362f348b9e560213b0b412d632bb5cf
ASMITA <asmitapawar3113@gmail.com>
2026-01-11 20:58:25 +0530

    11/01

---
+++ B_Drifting_Away.py
@@ -0,0 +1,14 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    s = input()
+    for i in range(len(s)-1):
+        if s[i]!="<" and s[i+1]!=">":
+            print("-1")
+            break
+    else:
+        print(len(s)-min( s.count("<") ,s.count(">")))

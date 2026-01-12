002e80a5bbe40ac9c253a4ac76edf25d6603c192
ASMITA <asmitapawar3113@gmail.com>
2026-01-12 20:54:36 +0530

    12/01

--- A_Social_Experiment.py
+++ A_Social_Experiment.py
@@ -8,8 +8,9 @@ for _ in range(int(input())):
     n = int(input())
     if n == 2:
         print(2)
-    elif n % 2 == 0 or n % 3 == 0:
+    elif n == 3:
+        print(3)
+    elif n % 2 == 0:
         print(0)
     else:
         print(1)
-

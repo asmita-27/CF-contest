f96edf14ad6a08dda51067b2b356dbd1bdce5e98
ASMITA <asmitapawar3113@gmail.com>
2026-01-12 20:55:34 +0530

    12/01

--- C_Huge_Pile.py
+++ C_Huge_Pile.py
@@ -5,5 +5,24 @@ import sys
 
 
 for _ in range(int(input())):
-    pass
+    n, k = map(int, input().split())
 
+    if k > n:
+        print(-1)
+        continue
+    time = 0
+    x = n
+    while x > k:
+        a = x // 2
+        b = x - a
+        if a >= k:
+            x = a
+        elif b >= k:
+            x = b
+        else:
+            time = -1
+            break
+
+        time += 1
+
+    print(time)

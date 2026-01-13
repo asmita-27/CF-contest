216ff28b39e895bc8d7d938f0f749660d6a2cea1
ASMITA <asmitapawar3113@gmail.com>
2026-01-13 18:25:21 +0530

    13/01

--- C_Huge_Pile.py
+++ C_Huge_Pile.py
@@ -3,26 +3,30 @@ import os
 import math
 import sys
 
-
-for _ in range(int(input())):
+def main():
     n, k = map(int, input().split())
 
-    if k > n:
-        print(-1)
-        continue
+    if n==k:
+        print(0)
+    cnt =0
+    flg = False
+    if n&1 :
+        flg = True
+    while n >= k:
+        if n & 1:
+            flg = True
 
-    time = 0
-    x = n
+        n //= 2
+        cnt += 1
 
-    while x > k:
-        half = x // 2
+        if n == k:
+            print(cnt)
+            return
+        if flg:
+            if n + 1 == k:
+                print(cnt)
+                return
+    print(-1)
 
-        if k > half:
-            x = x - half     
-        else:
-            x = half   
-        time += 1
-    if x == k:
-        print(time)
-    else:
-        print(-1)
+for _ in range(int(input())):
+    main()

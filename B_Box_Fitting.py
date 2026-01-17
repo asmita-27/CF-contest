7dc1e5eb3d5b48faa6ec0eddc739a52b7d1ae736
ASMITA <asmitapawar3113@gmail.com>
2026-01-17 10:13:51 +0530

    117/01

---
+++ B_Box_Fitting.py
@@ -0,0 +1,10 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n , w = map(int,input().split())
+    a = list(map(int,input().split()))
+    

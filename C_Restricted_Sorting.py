d5212044f3bb58240efe3e8c237d17d61466a58e
ASMITA <asmitapawar3113@gmail.com>
2026-01-29 20:51:57 +0530

    29/01

--- C_Restricted_Sorting.py
+++ C_Restricted_Sorting.py
@@ -5,5 +5,16 @@ import sys
 
 
 for _ in range(int(input())):
-    pass
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = sorted(a)
+    if a == b:
+        print(-1)
+        continue
 
+    ans = 0
+    for i in range(n):
+        if a[i] != b[i]:
+            ans = max(ans, abs(a[i] - b[i]))
+
+    print(ans)

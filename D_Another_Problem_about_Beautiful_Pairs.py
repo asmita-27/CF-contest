9c124c4e495d3620fa76af8cf64e8b1591526977
ASMITA <asmitapawar3113@gmail.com>
2026-02-11 21:21:13 +0530

    11/02

--- D_Another_Problem_about_Beautiful_Pairs.py
+++ D_Another_Problem_about_Beautiful_Pairs.py
@@ -4,17 +4,15 @@ import math
 import sys
 
 
+
 for _ in range(int(input())):
     n = int(input())
-    a = list(map(int, input().split()))
-    a = [0]+ a
+    a = [0] + list(map(int, input().split()))
     cnt = 0
     for i in range(1, n+1):
         k = a[i]
-        for j in range(k,n+1,k):
-            x = i+j
-            if x>n:
-                break
-            if a[x]==j//k:
-                cnt+=1
+        for j in range(k, n - i + 1, k):
+            x = i + j
+            if a[x] == j // k:
+                cnt += 1
     print(cnt)

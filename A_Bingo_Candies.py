a962d0c1daf763d5cc7104f29491988e7c9905a6
ASMITA <asmitapawar3113@gmail.com>
2026-03-17 19:55:28 +0530

    17/03

--- A_Bingo_Candies.py
+++ A_Bingo_Candies.py
@@ -5,5 +5,18 @@ import sys
 
 
 for _ in range(int(input())):
-    pass
+    n = int(input())
+    cnt = {}
+    for _ in range(n):
+        row = list(map(int, input().split()))
+        for x in row:
+            cnt[x] = cnt.get(x, 0) + 1
 
+    limit = n * (n - 1)
+    ok = True
+    for v in cnt.values():
+        if v > limit:
+            ok = False
+            break
+
+    print("YES" if ok else "NO")

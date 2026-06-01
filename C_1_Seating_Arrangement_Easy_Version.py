23585c14fb2f42290632e38a838fd7d2300c851f
ASMITA <asmitapawar3113@gmail.com>
2026-06-01 10:50:45 +0530

    01/06

--- C_1_Seating_Arrangement_Easy_Version.py
+++ C_1_Seating_Arrangement_Easy_Version.py
@@ -5,45 +5,29 @@ import sys
 
 
 for _ in range(int(input())):
-    n,x,s = map(int,input().split())
-    a = input().strip()
-    # new = 0
-    # free = 0
-    # ans = 0
-    # for ch in a:
-    #     if ch == 'I':
-    #         if new < x:
-    #             new += 1
-    #             free += s - 1
-    #             ans += 1
-    #     elif ch == 'E':
-    #         if free:
-    #             free -= 1
-    #             ans += 1
-    #     else: 
-    #         if free:
-    #             free -= 1
-    #             ans += 1
-    #         elif new < x:
-    #             new += 1
-    #             free += s - 1
-    #             ans += 1
-    # print(ans)
-    dp = [-1]*(x+1)
-    dp[0] = 0 
-    for i in a:
-        new = dp[:]
-        for j in range(x+1):
-            free = dp[j]
-            if free==-1:
-                continue 
-            if i !="E" and j < x:
-                new[j+1] = max(new[j+1], free)
-            if i !="I" and free < j*(s-1):
-                new[j] = max(new[j], free+1)
-        dp = new
-    res = 0
-    for i in range(x+1):
-        if dp[i]!=-1:
-            res = max(res, i+dp[i])
-    print(res)
+    n, t, s = map(int, input().split())
+    S = input().strip()
+    l = 0
+    r = 0
+    ans = 0
+    for c in S:
+        if c == 'A':
+            if t * s == ans:
+                continue
+            ans += 1
+            if ans > l * s:
+                l += 1
+            r = min(t, r + 1)
+        elif c == 'I':
+            if l == t:
+                continue
+            ans += 1
+            l += 1
+            r = min(t, r + 1)
+        elif c == 'E':
+            if ans == r * s:
+                continue
+            ans += 1
+            if ans > l * s:
+                l += 1
+    print(ans)

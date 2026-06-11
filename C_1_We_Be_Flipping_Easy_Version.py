7d3cf9c0b4e131f9b476386e4d8c80e7f0f151f3
ASMITA <asmitapawar3113@gmail.com>
2026-06-11 12:21:55 +0530

    11/06

---
+++ C_1_We_Be_Flipping_Easy_Version.py
@@ -0,0 +1,18 @@
+from collections import defaultdict, Counter, deque
+import os
+import math
+import sys
+
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    x = 0
+    ans = []
+    for i in reversed(range(n)):
+        x = -a[i] if x else a[i]
+        if x > 0:
+            ans.append(i)
+            x ^= 1
+    print(len(ans))
+    print(*(i + 1 for i in ans))

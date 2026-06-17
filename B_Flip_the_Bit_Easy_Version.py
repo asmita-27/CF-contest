b3c70307a8e2672b72e6e7b0f4a9defb0eb30337
ASMITA <asmitapawar3113@gmail.com>
2026-06-17 18:32:33 +0530

    17/06

--- B_Flip_the_Bit_Easy_Version.py
+++ B_Flip_the_Bit_Easy_Version.py
@@ -5,5 +5,16 @@ import sys
 
 
 for _ in range(int(input())):
-    pass
-
+    n,k = map(int,input().split())
+    a = list(map(int,input().split()))
+    p = int(input())
+    x = [a[p - 1]] + a + [a[p - 1]]
+    l = 0
+    r = 0
+    for i in range(p):
+        if x[i] != x[i + 1]:
+            l += 1
+    for i in range(p, n + 1):
+        if x[i] != x[i + 1]:
+            r += 1  
+    print(max(l, r))
